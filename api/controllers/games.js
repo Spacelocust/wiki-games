4bdde6ea6ba1ad870afbc16d887d9923bbe16418
spacelocust <lucasboganin@hotmail.fr>
2022-02-13 12:27:39 +0100

    api:auth[form](handling: validation & request) && games routing

--- api/controllers/games.js
+++ api/controllers/games.js
@@ -1,44 +1,88 @@
 import api from '../axiosBase.js';
-import { IMG_GAMES } from "../constant.js";
+import { IMG_GAMES } from '../constant.js';
 
 export const getGames = async (req, res) => {
     try {
         let response = [];
-        const { data } = await api.get('/videogames')
+        const { data } = await api.get('/videogames');
         res.set({
             'Content-Type': 'application/json',
-            'Cache-Control': 'max-age: 60',
+            'Cache-Control': 'max-age: 60'
         });
-        response = data?.reduce((acc, curr) => [...acc, { ...curr, ['img_url']: IMG_GAMES[curr.slug] }], [])
+        response = data?.reduce((acc, curr) => [...acc, { ...curr, ['img_url']: IMG_GAMES[curr.slug] }], []);
         res.send(response);
     } catch (e) {
         res.status(500).send(e);
     }
-}
+};
 
 export const getGame = async (req, res) => {
     try {
         const { data } = await api.get(`/videogames/${req.params.id}`);
         res.set({
             'Content-Type': 'application/json',
-            'Cache-Control': 'max-age: 60',
+            'Cache-Control': 'max-age: 60'
         });
         res.send({ ...data, ['img_url']: IMG_GAMES[data.slug] });
     } catch (e) {
         res.status(500).send(e);
     }
-}
+};
 
 export const getGameMatches = async (req, res) => {
     try {
-        const { data } = await api.get(`/matches?filter[videogame]=${req.params.id}`)
+        const { data } = await api.get(`/matches?filter[videogame]=${req.params.id}`);
         res.set({
             'Content-Type': 'application/json',
-            'Cache-Control': 'max-age: 60',
+            'Cache-Control': 'max-age: 60'
         });
         res.send(data);
     } catch (e) {
         res.status(500).send(e);
     }
-}
+};
+
+export const  getGamePastMatches = async (req, res) => {
+    const { id, page, per_page } = req.params;
+    try {
+        const { data } = await api.get(`/matches/past/?filter[videogame]=${id}&page=${page}&per_page=${per_page}`);
+        res.set({
+            'Content-Type': 'application/json',
+            'Cache-Control': 'max-age: 60'
+        });
+        res.send(data);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+};
+
+export const getGameRunningMatches = async (req, res) => {
+    const { id, page, per_page } = req.params;
+    try {
+        const { data } = await api.get(`/matches/running/?filter[videogame]=${id}&page=${page}&per_page=${per_page}`);
+        res.set({
+            'Content-Type': 'application/json',
+            'Cache-Control': 'max-age: 60'
+        });
+        res.send(data);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+};
+
+export const getGameUpcommingMatches = async (req, res) => {
+    const { id, page, per_page } = req.params;
+    try {
+        const { data } = await api.get(`/matches/upcoming/?filter[videogame]=${id}&page=${page}&per_page=${per_page}`);
+        res.set({
+            'Content-Type': 'application/json',
+            'Cache-Control': 'max-age: 60'
+        });
+        res.send(data);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+};
+
+
 
--- api/routes/games.js
+++ api/routes/games.js
@@ -1,12 +1,22 @@
 import express from 'express';
 const router = express.Router();
 
-import { getGames, getGame, getGameMatches } from '../controllers/games.js';
+import {
+    getGames,
+    getGame,
+    getGameMatches,
+    getGamePastMatches,
+    getGameRunningMatches,
+    getGameUpcommingMatches
+} from '../controllers/games.js';
 
 const baseURL = '/games';
 
 router.get(baseURL, getGames);
 router.get(`${baseURL}/:id`, getGame);
 router.get(`${baseURL}/:id/matches`, getGameMatches);
+router.get(`${baseURL}/:id/matches/past/:page/:per_page`, getGamePastMatches);
+router.get(`${baseURL}/:id/matches/running/:page/:per_page`, getGameRunningMatches);
+router.get(`${baseURL}/:id/matches/upcoming/:page/:per_page`, getGameUpcommingMatches);
 
 export default router;
