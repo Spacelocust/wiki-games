93264bdf542f9df54a05d7973c29b01e8e68a963
spacelocust <lucasboganin@hotmail.fr>
2022-02-18 20:45:09 +0100

    api:routes[games](refactoring names routes)

--- api/routes/games.js
+++ api/routes/games.js
@@ -14,9 +14,9 @@ const baseURL = '/games';
 
 router.get(baseURL, getGames);
 router.get(`${baseURL}/:id`, getGame);
-router.get(`${baseURL}/:id/matches`, getGameMatches);
-router.get(`${baseURL}/:id/matches/past/:page/:per_page`, getGamePastMatches);
-router.get(`${baseURL}/:id/matches/running/:page/:per_page`, getGameRunningMatches);
-router.get(`${baseURL}/:id/matches/upcoming/:page/:per_page`, getGameUpcommingMatches);
+router.get(`${baseURL}/:id/matchs`, getGameMatches);
+router.get(`${baseURL}/:id/matchs/past/:page/:per_page`, getGamePastMatches);
+router.get(`${baseURL}/:id/matchs/running/:page/:per_page`, getGameRunningMatches);
+router.get(`${baseURL}/:id/matchs/upcoming/:page/:per_page`, getGameUpcommingMatches);
 
 export default router;
